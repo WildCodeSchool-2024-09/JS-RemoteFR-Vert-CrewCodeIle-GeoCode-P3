82a693c53b860ae35d7bbb3072bc822899a26608
REMI DUBUS <remdubus@gmail.com>
2025-01-08 12:26:22 +0100

    feat: finish the navbar closes #29

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -17,18 +17,24 @@ export default function NavBar() {
       <img src={logo} alt="logo" className="hidden lg:inline" />
       <ul className="h-full flex items-center justify-around w-11/12 rounded-full shadow-md shadow-darkColor bg-darkColor lg:bg-transparent lg:shadow-none lg:w-1/2">
         {navBarData.map((e, i) => (
-          <Link to={e.url} key={e.id}>
-            <img alt={e.name} src={navBarIcons[i]} className="h-8 lg:hidden" />
-            <li
-              className={
-                e.name === navBarData[0].name
-                  ? "lg:hidden"
-                  : "hidden lg:inline-block lg:bg-interestColor lg:py-3 lg:min-w-28 lg:max-w-28 lg:m-0 lg:text-center lg:rounded-full lg:shadow-md lg:shadow-darkColor lg:font-title lg:text-lightColor lg:active:bg-darkColor"
-              }
-            >
-              {e.name !== navBarData[0].name && e.name}
-            </li>
-          </Link>
+          <li key={e.id}>
+            <Link to={e.url}>
+              <img
+                alt={e.name}
+                src={navBarIcons[i]}
+                className="h-8 lg:hidden"
+              />
+              <p
+                className={
+                  e.name === navBarData[0].name
+                    ? "lg:hidden"
+                    : "hidden lg:inline-block lg:bg-interestColor lg:py-3 lg:min-w-28 lg:max-w-28 lg:m-0 lg:text-center lg:rounded-full lg:shadow-md lg:shadow-darkColor lg:font-title lg:text-lightColor lg:active:bg-darkColor"
+                }
+              >
+                {e.name !== navBarData[0].name && e.name}
+              </p>
+            </Link>
+          </li>
         ))}
       </ul>
     </nav>
