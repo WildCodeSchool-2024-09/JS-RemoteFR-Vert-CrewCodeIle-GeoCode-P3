bb62925d898832c0a14432a626626338ee11ee81
Flam64 <celio.mozes.rocha@gmail.com>
2025-01-10 10:40:26 +0100

    feat: us39 added map with arbitrary position

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,8 @@
 import { Outlet } from "react-router-dom";
-import NavBar from "./components/NavBar";
-//import HomePage from "./pages/HomePage";
 
 export default function App() {
   return (
     <>
-      {/* <HomePage /> */}
-      <NavBar />
       <Outlet />
     </>
   );
