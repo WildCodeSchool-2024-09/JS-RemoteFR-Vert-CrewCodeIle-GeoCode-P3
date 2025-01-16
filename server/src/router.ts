79282585dc38c177b2951a064c50336a6c69eaa8
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-01-16 17:55:36 +0100

    feat: add router repository

--- server/src/router.ts
+++ server/src/router.ts
@@ -7,28 +7,11 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Define item-related routes
-import itemActions from "./modules/item/itemActions";
-
-router.get("/api/items", itemActions.browse);
-router.get("/api/items/:id", itemActions.read);
-router.post("/api/items", itemActions.add);
 
 // Defin register-related routes
-import registerAction from "./modules/item/registerAction";
-router.get("/api/register", registerAction.browseBrand);
-router.get("/api/register/mail", registerAction.browseMail);
-router.get("/api/register/:id", registerAction.readModel);
-router.get("/api/register/socket/:id", registerAction.readSocket);
-router.post(
-  "/api/register",
-  registerAction.validateUser,
-  registerAction.addUserInfo,
-);
-router.post(
-  "/api/register/vehicule",
-  registerAction.validateVehicule,
-  registerAction.addVehicleInfo,
-);
+import RegisterRouter from "./routes/formRegister.routes";
+
+router.use("/", RegisterRouter);
 
 /* ************************************************************************* */
 
