36debfaecc91962b20349250d850a7deb57bf655
REMI DUBUS <remdubus@gmail.com>
2025-01-22 14:43:22 +0100

    fix: route front closes #2

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,6 +2,8 @@ import App from "./App";
 
 import { createBrowserRouter } from "react-router-dom";
 
+import ModalRegistration from "./components/ModalRegistration";
+
 import HomePage from "./pages/HomePage";
 import PresentationPage from "./pages/PresentationPage";
 
@@ -16,6 +18,10 @@ export const mainRouter = createBrowserRouter([
       {
         path: "/home",
         element: <HomePage />,
+      },
+      {
+        path: "/home/formulaire",
+        element: <ModalRegistration />,
       },
     ],
   },
